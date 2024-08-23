c40cdbf21fa4385568647e029fe144ddd17c0b99
Shiva Sai Charan Dodda <shivasaicharandodda@gmail.com>
2024-08-23 13:15:40 +0530

    routes

---
+++ backend/model/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import {module} from "node";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 3,
+    }
+  },
+  
+);
+
+module.exports = mongoose.model("User", userSchema);
