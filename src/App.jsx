f4c5ea588513a5c28ff0a31f9b8ebc167dfcef83
Shiva Sai Charan Dodda <shivasaicharandodda@gmail.com>
2024-08-30 21:15:19 +0530

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@
 import './App.css'
 import Home from './components/Home.jsx'
 import NavbarSimple from './components/NavbarSimple.jsx'
-import Theatre from './components/Theatre.jsx'
+import TheatreSeat from './components/TheatreSeat.jsx'
 import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx'
+import Theatre from './components/Theatre.jsx'
 import UserDashboard from './components/UserDashboard.jsx';
 import { Routes, Route , useNavigate} from 'react-router-dom'
 import { useState, useEffect } from 'react'
@@ -35,14 +36,13 @@ function App() {
        <NavbarSimple isLoggedIn={isLoggedIn} onLogout={handleLogout} />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/theatre' element={<Theatre />} />
+        <Route path='/theatreSeat' element={<TheatreSeat />} />
         <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/dashboard' element={<UserDashboard />} />
+        <Route path='/theatre' element={<Theatre />} />
       </Routes>
       {/* <Theatre /> */}
-
-
     </>
   )
 }
